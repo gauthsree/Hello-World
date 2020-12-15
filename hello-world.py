2cb9937a0fc87b1f5481913a3e74eef9e9eb7c19
subeesh <subeeshoceanography@gmail.com>
2020-12-16 00:50:27 +0530

    hello world from subeesh

--- hello-world.py
+++ hello-world.py
@@ -4,4 +4,4 @@ print("Hi, Gautham. Welcome to python scripting !")
 print("This is Vineet, Hello everyone")
 print(" Hi its Jenson, sorry late to party!!")
 print("Hi everyone, this is Jithin..")
-
+print("Hi Subeesh here .. Last bencher!!")
