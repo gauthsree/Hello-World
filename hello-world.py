d33c33f3abe5c2d5fe4234bbaa34b25a9f66729c
Jithin <jithinoceanography@gmail.com>
2020-12-15 21:08:33 +0530

    Message

--- hello-world.py
+++ hello-world.py
@@ -3,4 +3,5 @@
 print("Hi, Gautham. Welcome to python scripting !")
 print("This is Vineet, Hello everyone")
 print(" Hi its Jenson, sorry late to party!!")
+print("Hi everyone, this is Jithin..")
 
